3f675514b6c95ecfc3a354cde81ace9cd3a402f2
Jay <chawreyj@gmail.com>
2021-03-29 13:07:17 +0530

    First demo code changes

--- firstDemo.cc
+++ firstDemo.cc
@@ -23,8 +23,8 @@
 #include "ns3/csma-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/yans-wifi-helper.h"
-#include "ns3/ssid.h"
-
+#include "ns3/ssid.h" 
+#include "ns3/netanim-module.h"
 //   Wifi 10.1.3.0
 //
 //    *     *     *
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
     bool verbose = true;
     //uint32_t nCsma = 3;
     uint32_t nWifi = 2;
-    bool tracing = false;
+    bool tracing = true;
 
     CommandLine cmd(__FILE__);
 
@@ -126,11 +126,11 @@ int main(int argc, char *argv[])
 
     Ipv4AddressHelper address;
 
-    address.SetBase("10.1.1.0", "255.255.255.0");
+    address.SetBase("10.1.1.0", "255.255.255.0"); //10.1.1.2 remote
     Ipv4InterfaceContainer p2pInterfaces;
     p2pInterfaces = address.Assign(p2pDevices);
 
-    address.SetBase("10.1.3.0", "255.255.255.0");
+    address.SetBase("10.1.3.0", "255.255.255.0");  //10.1.3.1 laptop , 10.1.3.2 mobile
     Ipv4InterfaceContainer wifiNodesInterfaces;
     Ipv4InterfaceContainer apNodeInterface;
 
@@ -153,21 +153,42 @@ int main(int argc, char *argv[])
     clientApps.Start(Seconds(2.0));
     clientApps.Stop(Seconds(10.0));
     // //client 2
-    echoClient.Install(wifiStaNodes.Get(nWifi - 2));
-    clientApps.Start(Seconds(2.0));
-    clientApps.Stop(Seconds(10.0));
+   
+    ApplicationContainer client1Apps = echoClient.Install(wifiStaNodes.Get(nWifi - 2));
+    client1Apps.Start(Seconds(3.0));
+    client1Apps.Stop(Seconds(10.0));
+
+    //Laptop to Mobile communcation
+    UdpEchoServerHelper echo1Server(13);
+
+    ApplicationContainer server1Apps = echo1Server.Install(wifiStaNodes.Get(0));
+    server1Apps.Start(Seconds(11.0));
+    server1Apps.Stop(Seconds(20.0));
+
+    UdpEchoClientHelper echo1Client(wifiNodesInterfaces.GetAddress(0), 13);
+    echo1Client.SetAttribute("MaxPackets", UintegerValue(1));
+    echo1Client.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echo1Client.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer client3Apps = echo1Client.Install(wifiStaNodes.Get(1));
+    client3Apps.Start(Seconds(12.0));
+    client3Apps.Stop(Seconds(20.0));
+    
 
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-    Simulator::Stop(Seconds(10.0));
+    Simulator::Stop(Seconds(20.0));
 
     if (tracing == true)
     {
-        pointToPoint.EnablePcapAll("HomeNetwork");
-        phy.EnablePcap("HomeNetwork", apDevices.Get(0));
+        pointToPoint.EnablePcapAll("Home_Network",p2pDevices.Get(0));
+        phy.EnablePcap("Home_Network", apDevices.Get(0));
+        phy.EnablePcap("Home_Network", staDevices.Get(0));
         //csma.EnablePcap ("HomeNetwork", csmaDevices.Get (0), true);
     }
-
+    AnimationInterface anim ("HomeNetwork.xml");
+    anim.SetConstantPosition (p2pNodes.Get (1), 10.0, 20.0);
+    anim.SetConstantPosition (p2pNodes.Get (0), 3.0, 3.0);
     Simulator::Run();
     Simulator::Destroy();
     return 0;
